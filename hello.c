912dc78ba7a3810c8c3d87597ed46b11039ad404
Jvaughn9 <142941884+Jvaughn9@users.noreply.github.com>
2023-08-25 16:42:30 -0500

    Added name and major

--- hello.c
+++ hello.c
@@ -4,4 +4,7 @@ int main(int argc, char **argv)
 {
     printf("William Aldag!\n");
     printf("My major is Computer Science\n");
+    printf("Jake Vaughn.\n");
+    printf("My major is Data Science\n");
+    
 }
